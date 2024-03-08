cd756beb514fb73ca6ef65a38cbbc022f7542fb3
SHAIK ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-03-08 15:48:18 +0530

    Differentiation of client and server

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,8 +1,9 @@
-"use client"
+
+
+import {ButtonCom} from "./Button";
+
 export const SigninCompo = () => {
-  function Handle() {
-    console.log("hi");
-  }
+  
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -25,13 +26,8 @@ export const SigninCompo = () => {
                 placeholder="123456"
               />
 
-              <button
-                type="button"
-                onClick={Handle}
-                className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              >
-                Sign in
-              </button>
+              
+                <ButtonCom/>
             </div>
           </div>
         </a>
