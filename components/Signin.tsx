e3d4f3a45f4e43d9e48b8d6cdb75776e0c770b65
SHAIK ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-03-08 15:20:08 +0530

    Learning client and server side components

--- components/Signin.tsx
+++ components/Signin.tsx
@@ -1,4 +1,8 @@
-export default function Signin() {
+"use client"
+export const SigninCompo = () => {
+  function Handle() {
+    console.log("hi");
+  }
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -20,8 +24,10 @@ export default function Signin() {
                 type={"password"}
                 placeholder="123456"
               />
+
               <button
                 type="button"
+                onClick={Handle}
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
                 Sign in
@@ -32,7 +38,7 @@ export default function Signin() {
       </div>
     </div>
   );
-}
+};
 
 interface LabelledInputType {
   label: string;
